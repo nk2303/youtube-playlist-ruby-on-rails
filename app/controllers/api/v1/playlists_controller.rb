8e37889005a38924b5e2e6ad6126af991078fa39
nk2303 <ngankhong03@gmail.com>
2020-06-27 16:24:38 -0700

    render all playlists except for playlist from current users

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::PlaylistsController < ApplicationController
     end
 
     def all_playlists
-        all_playlists = Playlist.all
+        current_user_playlists_id = current_user.playlists.map{|p_obj| p_obj.id}
+        all_playlists = Playlist.all.select { |p| !current_user_playlists_id.include? p.id }
         render json: all_playlists, status: :ok
     end
 
